88cac062ee614d799f3cf007052a7746d9e51dd3
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-12-06 14:33:04 +0530

    math server express

---
+++ web-dev/http-server/mathServer.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/sum", (req, res) => {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  return res.status(200).json({ sum: a + b });
+});
+
+app.get("/subtract", (req, res) => {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  return res.status(200).json({ difference: a - b });
+});
+
+app.get("/multiply", (req, res) => {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  return res.status(200).json({ product: a * b });
+});
+
+app.get("/divide", (req, res) => {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  if (b === 0) throw new Error("Arithmatic Error: Division by Zero");
+  return res.status(200).json({ division: a / b });
+});
+
+app.use((err, req, res, next) => {
+  if (err) {
+    return res.status(400).json({ message: "Probably some bad inputs" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running at port 3000`);
+});
