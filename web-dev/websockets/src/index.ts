518a9e4b4ad8dfab43d952eea8129b2b8a86fcc4
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2026-01-08 22:38:03 +0530

    backend for chat app

--- web-dev/websockets/src/index.ts
+++ web-dev/websockets/src/index.ts
@@ -1,14 +1,59 @@
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws) => {
-  console.log("Client connected");
-  setInterval(() => {
-    ws.send("Current time is " + new Date().toISOString());
-  }, 1000);
+let allSockets: Map<string, WebSocket[]> = new Map();
 
-  ws.on("message", (message) => {
-    console.log(message.toString());
+wss.on("connection", (socket) => {
+  socket.on("error", (e) => console.log(e.message));
+
+  socket.on("message", (e) => {
+    const parsedEvent = JSON.parse(e as unknown as string);
+
+    if (parsedEvent.type === "join") {
+      if (allSockets.get(parsedEvent.payload.roomId)) {
+        allSockets.get(parsedEvent.payload.roomId)?.push(socket);
+      } else {
+        allSockets.set(parsedEvent.payload.roomId, [socket]);
+      }
+    }
+
+    if (parsedEvent.type === "chat") {
+      const socketsToSend = allSockets
+        .get(parsedEvent.payload?.roomId)
+        ?.filter((s) => s !== socket);
+      socketsToSend?.forEach((s) => s.send(parsedEvent.payload.message));
+    }
+  });
+
+  socket.on("close", (e) => {
+    const parsedEvent = JSON.parse(e.toString());
+    const updatedSockets = allSockets
+      .get(parsedEvent.payload?.roomId)
+      ?.filter((x) => x !== socket);
+
+    if (!updatedSockets?.length) allSockets.delete(parsedEvent.payload.roomId);
+    else allSockets.set(parsedEvent.payload?.roomId, updatedSockets);
   });
 });
+
+//! Global Broadcasting Chat
+// let userCount = 0;
+// let allSockets: WebSocket[] = [];
+
+// wss.on("connection", (ws) => {
+//   allSockets.push(ws);
+//   ws.on("error", (e) => console.log(e.message));
+//   userCount++;
+//   console.log(`${userCount} user(s) connected`);
+
+//   ws.on("message", (message) => {
+//     console.log(message.toString());
+//     allSockets.forEach((s) => s.send(message.toString()));
+//   });
+
+//   ws.on("close", () => {
+//     allSockets = allSockets.filter((s) => s !== ws);
+//     userCount--;
+//   });
+// });
