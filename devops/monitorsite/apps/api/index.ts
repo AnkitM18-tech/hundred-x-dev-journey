3a26bc629df7c1352772ef3a73f9ed502f44da14
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2026-04-06 21:50:46 +0530

    monitorsite and monitorsite-rust

---
+++ devops/monitorsite/apps/api/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.post("/website", (req, res) => {});
+
+app.get("/status/:websiteId", (req, res) => {});
+
+app.listen(process.env.PORT || 3000, () => console.log("Backend is Up!"));
